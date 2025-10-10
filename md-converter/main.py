c1dffddebff830459043b6f7301846acde5135ff
Tony Ebel <tonyebel123@gmail.com>
2025-10-10 14:18:53 -0500

    structure of message data and attributes were wrong

--- md-converter/main.py
+++ md-converter/main.py
@@ -20,22 +20,20 @@ def html_to_gcs(html, job_id, bucket_name):
 
 @functions_framework.cloud_event
 def pubsub_trigger(cloud_event):
+    job_id = None
+    bucket_name = None
     try:
-        pubsub_message = cloud_event.data["message"]
+        attributes = cloud_event.data['message']['attributes']
 
-        if pubsub_message.get('data'):
-            data_bytes = base64.b64decode(pubsub_message['data'])
-            data_str = data_bytes.decode('utf-8')
+        job_id = attributes.get("jobid")
+        bucket_name = attributes.get("bucketname")
 
-            payload = json.loads(data_str)
+    except (KeyError, AttributeError):
+        print('Message attributes not found or incorrectly structured.')
 
-            markdown = payload.get("markdown", None)
-            job_id = payload.get("jobid", None)
-            bucket_name = payload.get("bucketname", None)
+    markdown = None
+    if cloud_event.data['message']['data']:
+        markdown = base64.b64decode(cloud_event.data['message']['data']).decode('utf-8')
 
-            html = markdown_to_html(markdown)
-            html_to_gcs(html, job_id, bucket_name)
-
-
-    except Exception as e:
-        print(f'An error occured: {e}')
+    html = markdown_to_html(markdown)
+    html_to_gcs(html, job_id, bucket_name)
