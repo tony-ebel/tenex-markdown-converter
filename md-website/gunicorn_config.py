9875c39a1bffdde9e2e84f9715c7735389c0022f
Tony Ebel <tonyebel123@gmail.com>
2025-10-11 13:33:48 -0500

    switch to gunicorn config file

---
+++ md-website/gunicorn_config.py
@@ -0,0 +1,15 @@
+import os
+from multiprocessing import cpu_count
+
+workers = 2
+max_requests = 100
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+if ENVIRONMENT != 'local':
+  workers = cpu_count() * 2 + 1
+  max_requests = 1000
+
+bind = f'0.0.0.0:{os.getenv("PORT", "8500")}'
+loglevel = 'info'
+accesslog = '-'
+errorlog = '-'
